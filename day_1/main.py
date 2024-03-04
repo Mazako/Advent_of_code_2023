7a3ed57e02920f067d63f7b99f489bc1686dee90
michał <michal.maziarz12@gmail.com>
2024-03-04 08:13:42 +0100

    day 1, part 2, step 1

--- day_1/main.py
+++ day_1/main.py
@@ -8,4 +8,21 @@ def part_1():
         print(total)
 
 
-
+def part_2():
+    with open('example.txt', 'r') as file:
+        lines = file.readlines()
+        total = 0
+        numbers = []
+        for line in lines:
+            i = 0
+            arr = []
+            while i < len(line):
+                if line[i: i + 3] == 'one':
+                    arr.append(1)
+                    i += 3
+                elif line[i: i + 3] == 'two':
+                    arr.append(2)
+                    i += 3
+                elif line [i : i + 4] == 'three':
+                    arr.append(3)
+                    i += 4
