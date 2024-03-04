14c222758c48607640c429218ccd8946c5c982b8
Mazak <michal.maziarz12@gmail.com>
2024-03-04 11:33:47 +0100

    day 1, part 2

--- day_1/main.py
+++ day_1/main.py
@@ -5,24 +5,51 @@ def part_1():
         for line in lines:
             nums = list(filter(lambda x: x.isnumeric(), line.strip('')))
             total += int(nums[0] + nums[-1])
-        print(total)
+        return total
 
 
 def part_2():
-    with open('example.txt', 'r') as file:
+    with open('input.txt', 'r') as file:
         lines = file.readlines()
         total = 0
-        numbers = []
         for line in lines:
             i = 0
             arr = []
             while i < len(line):
                 if line[i: i + 3] == 'one':
-                    arr.append(1)
-                    i += 3
+                    arr.append('1')
+                    i += 2
                 elif line[i: i + 3] == 'two':
-                    arr.append(2)
-                    i += 3
-                elif line [i : i + 4] == 'three':
-                    arr.append(3)
+                    arr.append('2')
+                    i += 2
+                elif line[i: i + 5] == 'three':
+                    arr.append('3')
                     i += 4
+                elif line[i: i + 4] == 'four':
+                    arr.append('4')
+                    i += 3
+                elif line[i: i + 4] == 'five':
+                    arr.append('5')
+                    i += 3
+                elif line[i: i + 3] == 'six':
+                    arr.append('6')
+                    i += 2
+                elif line[i: i + 5] == 'seven':
+                    arr.append('7')
+                    i += 4
+                elif line[i: i + 5] == 'eight':
+                    arr.append('8')
+                    i += 4
+                elif line[i: i + 4] == 'nine':
+                    arr.append('9')
+                    i += 3
+                elif line[i].isnumeric():
+                    arr.append(line[i])
+                    i += 1
+                else:
+                    i += 1
+            total += int(arr[0] + arr[-1])
+    return total
+
+print(part_1())
+print(part_2())
